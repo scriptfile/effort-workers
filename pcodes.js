6c84f717d76629e2325e05cfe1b36d4947f26001
scriptfile <78675338+scriptfile@users.noreply.github.com>
2022-07-21 03:10:40 +0600

    Create pcodes.js

---
+++ pcodes.js
@@ -0,0 +1,47 @@
+var config = {
+  apiKey: "AIzaSyCWGbJi9qeD7h6BPhAKBoWupHv21Tge3ck",
+  authDomain: "effort-5efac.firebaseapp.com",
+  databaseURL: "https://effort-5efac.firebaseio.com",
+  projectId: "effort-5efac",
+  storageBucket: "effort-5efac.appspot.com",
+  messagingSenderId: "771931121287",
+  appId: "1:771931121287:web:9fe9abdcab7c369f277a22",
+  measurementId: "G-GLD9CY5VPF"
+};
+firebase.initializeApp(config);
+
+//////////////Ip Tricker///////////
+$.ajax({
+    url: 'https://api.ipify.org/?format=json',
+    dataType: 'json',
+    success: function( data ) {
+	  
+	  
+	  
+	   $.getJSON("https://api.ipify.org/?format=json", function(data) {
+        
+	document.getElementById("ip").innerHTML = (data.ip);
+		Update();
+		
+		});
+	  
+    },
+    error: function( data ) {
+      console.log(data)
+	  alert(0)
+    }
+  });
+	
+  
+  ///Make Guest & Pcode Update Firebase///
+  function Update(){
+  var ip = document.getElementById("ip").innerHTML;
+  var Guest = ip.replace(/\./g, "-");
+  var Fguest = "Topexpertbd/Guest-User/"+Guest;
+  var pcode = "<?php echo $pcode; ?>";     
+      
+firebase.database().ref(Fguest).update({ 
+    Pcode : pcode,
+	});
+}
+ 
